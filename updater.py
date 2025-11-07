e73c92b61c621b110f9f7eef2a47c2723f836a65
agireesh23comp-ux <agireesh23comp@student.mes.ac.in>
2025-11-07 14:55:00 +0530

    Delete updater.py

--- updater.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Load the old model
-model = tf.keras.models.load_model('tf-cnn-model.h5', compile=False)
-print("Model loaded successfully!")
-
-# Save it in the new format
-model.save('tf-cnn-model-new.h5')
-print("Model saved in new format!")
